4a2b9d1925217803a2d479d8b20cad18864086c8
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-04-13 11:52:03 +0200

    Started working on GUI

---
+++ Conversator/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using lib12.DependencyInjection;
+using lib12.WPF.Core;
+
+namespace Conversator
+{
+    [Singleton]
+    public class MainViewModel : NotifyingObject
+    {
+        #region Properties
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        private string userText;
+        public string UserText
+        {
+            get { return userText; }
+            set
+            {
+                userText = value;
+                OnPropertyChanged("UserText");
+            }
+        }
+        #endregion
+
+        public MainViewModel()
+        {
+            Text = "asdf";
+            UserText = "asdfasdf";
+        }
+    }
+}
