ef3da21654a5e894a8910e65d8fb3798edcf4c2d
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-04-13 12:41:56 +0200

    Started working on parsing

--- Conversator/ConversationEngine.cs
+++ Conversator/ConversationEngine.cs
@@ -1,12 +1,34 @@
-﻿using lib12.DependencyInjection;
+﻿using System.Windows.Controls;
+using lib12.DependencyInjection;
+using mshtml;
 
 namespace Conversator
 {
     [Singleton]
     public class ConversationEngine
     {
+        private const string ConversatorAddress = "http://www.cleverbot.com/";
+        private const string SayItButtonId = "sayit";
+        private const string SayItTextBoxId = "stimulus";
+
+        private readonly WebBrowser browser;
+        private readonly IHTMLDocument2 document;
+        private readonly dynamic sayItButton;
+        private readonly dynamic sayItTextBox;
+
+        public ConversationEngine()
+        {
+            browser = new WebBrowser();
+            browser.Navigate(ConversatorAddress);
+            document = (IHTMLDocument2)browser.Document;
+            sayItButton = document.all.item(SayItButtonId);
+            sayItTextBox = document.all.item(SayItTextBoxId);
+        }
+
         public string Say(string text)
         {
+            sayItTextBox.value = text;
+            sayItButton.click();
             return text + text;
         }
     }
